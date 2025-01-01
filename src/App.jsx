23db9f8f41236527f53a4091825d21520f9a3a1a
posaidon09 <halohalo090909@gmail.com>
2025-01-01 12:28:38 +0100

    woops

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import { useContext } from "react";
+import Root from "./pages/Root.jsx";
+import { context } from "./lib/Context.jsx";
+import Icon from "./components/Icon.jsx";
+
+function App() {
+    const { page } = useContext(context);
+    function getPage() {
+        if (page == 0) return <Root />
+        return <Root />
+    }
+      return (
+          <div>
+            {getPage()}
+          </div>
+      );
+}
+
+export default App;
---
+++ src/pages/Root.jsx
@@ -0,0 +1,7 @@
+export default function Root() {
+    return (
+        <div className="bg-black min-h-screen overflow-auto">
+            <h1 className="text-text-50 text-3xl text-center">template</h1>
+        </div>
+    );
+}
